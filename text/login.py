1cd5c943d71553c695a44a41e2bfd0a55390711a
genghua0123 <17710225818@163.com>
2018-04-15 14:17:23 +0800

    sjdfaisdhfs

--- text/login.py
+++ text/login.py
@@ -1,4 +1,9 @@
 ktop/zhangsan/test007
 编辑代码：num1 = 10
 本地仓库记录版本：git commit -am '第一个变量'
+推送到远程仓库：git pus
+进入经理本地仓库：cd Desktop/manager/test007/
+经理同步服务器代码：git pull
+编辑代码：num2 = 20
+本地仓库记录版本：git commit -am '第二个变量'
 推送到远程仓库：git push
