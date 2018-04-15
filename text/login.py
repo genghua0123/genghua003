0f0f5adfb5d1cfacf0dea57628b85e341dea5019
genghua0123 <17710225818@163.com>
2018-04-15 21:33:36 +0800

    djfaosdjfas

--- text/login.py
+++ text/login.py
@@ -1,28 +1,6 @@
-进入经理本地仓库：cd Desktop/manager/test007/
-经理同步服务器代码：git pull
-编辑代码：num2 = 20
-本地仓库记录版本：git commit -am '第二个变量'
-推送到远程仓库：git pushZZktop/zhangsan/test007
-编辑代码：num1 = 10
-本地仓库记录版本：git commit -am '第一个变量'
-fadhfkalsdhkalf
-dahsflkjasdf
-dflajdfas
-ajksdfas
-推送到远程仓库：git pus
-要使用git命令操作仓库，需要进入到仓库内部
-adfhalisdhfahsjdhfakjs
-sdbfasdhflakd
-要同步服务器代码就执行：git pull
-进入经理本地仓库：cd Desktop/manager/test007/
-经理同步服务器代码：git pull
-编辑代码：num2 = 20
-本地仓库记录版本：git commit -am '第二个变量'
-推送到远程仓库：git push本地仓库记录版本就执行：git commit -am '版本描述'
-推送代码到服务器就执行：git push
-编辑代码前要先pull，编辑完再
-进入经理本地仓库：cd Desktop/manager/test007/
-经理同步服务器代码：git pull
-编辑代码：num2 = 20
-本地仓库记录版本：git commit -am '第二个变量'
-推送到远程仓库：git push
+1 = 20
+
+3 = 10
+
+2 = 20
+
