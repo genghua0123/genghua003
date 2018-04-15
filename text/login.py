44d8248ce5eb9c79cd7c50bf54065a26de1d73d5
经理 <manage@135.com>
2018-04-15 14:19:35 +0800

    jiosdjfasd

--- text/login.py
+++ text/login.py
@@ -2,6 +2,11 @@ ktop/zhangsan/test007
 编辑代码：num1 = 10
 本地仓库记录版本：git commit -am '第一个变量'
 推送到远程仓库：git pus
+要使用git命令操作仓库，需要进入到仓库内部
+要同步服务器代码就执行：git pull
+本地仓库记录版本就执行：git commit -am '版本描述'
+推送代码到服务器就执行：git push
+编辑代码前要先pull，编辑完再
 进入经理本地仓库：cd Desktop/manager/test007/
 经理同步服务器代码：git pull
 编辑代码：num2 = 20
